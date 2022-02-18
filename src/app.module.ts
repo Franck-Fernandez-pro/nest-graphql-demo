67c1fc2e3d2e996ff9967897717037b12c56de86
Franck Fernandez <franck.fernandez@gmail.com>
2022-02-18 20:11:39 +0100

    Update GraphQLModule (new nestjs doc)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
     }),
     PetsModule,
   ],
