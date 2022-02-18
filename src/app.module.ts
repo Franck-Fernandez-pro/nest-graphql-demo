80d300835a662bc4cbb3b0ade7eb557d22a62b4c
Franck Fernandez <franck.fernandez@gmail.com>
2022-02-18 20:22:31 +0100

    Add TypeOrmModule config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,19 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     PetsModule,
   ],
