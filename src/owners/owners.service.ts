3793c56f38f60e736c389626eb67125ff3b64850
Franck Fernandez <franck.fernandez@gmail.com>
2022-02-19 12:21:40 +0100

    Create Owner services to findAll and findById

--- src/owners/owners.service.ts
+++ src/owners/owners.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateOwnerInput } from './dto/create-owner.input';
 import { UpdateOwnerInput } from './dto/update-owner.input';
+import { Owner } from './entities/owner.entity';
 
 @Injectable()
 export class OwnersService {
-  create(createOwnerInput: CreateOwnerInput) {
-    return 'This action adds a new owner';
+  constructor(
+    @InjectRepository(Owner) private ownersRepository: Repository<Owner>,
+  ) {}
+
+  create(createOwnerInput: CreateOwnerInput): Promise<Owner> {
+    const newOwner = this.ownersRepository.create(createOwnerInput);
+
+    return this.ownersRepository.save(newOwner);
   }
 
-  findAll() {
-    return `This action returns all owners`;
+  findAll(): Promise<Owner[]> {
+    return this.ownersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} owner`;
+  findOne(id: number): Promise<Owner> {
+    return this.ownersRepository.findOneOrFail(id);
   }
 
   update(id: number, updateOwnerInput: UpdateOwnerInput) {
