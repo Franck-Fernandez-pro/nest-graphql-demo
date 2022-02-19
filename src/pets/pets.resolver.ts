b3e6384e2bd777a50f65f4748a25d4f0a89182eb
Franck Fernandez <franck.fernandez@gmail.com>
2022-02-19 12:18:11 +0100

    Remove unused fn arg

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -3,21 +3,21 @@ import { CreatePetInput } from './dto/create-pet.input';
 import { Pet } from './pet.entity';
 import { PetsService } from './pets.service';
 
-@Resolver((of) => Pet)
+@Resolver(() => Pet)
 export class PetsResolver {
   constructor(private petService: PetsService) {}
 
-  @Query((returns) => Pet)
+  @Query(() => Pet)
   getPet(@Args('id', { type: () => Int }) id: number): Promise<Pet> {
     return this.petService.findOne(id);
   }
 
-  @Query((returns) => [Pet])
+  @Query(() => [Pet])
   pets(): Promise<Pet[]> {
     return this.petService.findAll();
   }
 
-  @Mutation((returns) => Pet)
+  @Mutation(() => Pet)
   createPet(
     @Args('createPetInput') createPetInput: CreatePetInput,
   ): Promise<Pet> {
