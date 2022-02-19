9b6267d9bec4c0255bf3484876bcb335391dcb0b
Franck Fernandez <franck.fernandez@gmail.com>
2022-02-19 12:44:09 +0100

    Import OwnersService + Create getOwner method

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { OwnersService } from '../owners/owners.service';
 import { Repository } from 'typeorm';
 import { CreatePetInput } from './dto/create-pet.input';
 import { Pet } from './pet.entity';
+import { Owner } from '../owners/entities/owner.entity';
 
 @Injectable()
 export class PetsService {
-  constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) {}
+  constructor(
+    @InjectRepository(Pet) private petsRepository: Repository<Pet>,
+    private ownersService: OwnersService,
+  ) {}
 
   createPet(createPetInput: CreatePetInput): Promise<Pet> {
     // newPet = new Pet; newPet.name = ...;
@@ -22,5 +27,9 @@ export class PetsService {
 
   async findOne(id: number): Promise<Pet> {
     return this.petsRepository.findOneOrFail(id);
+  }
+
+  getOwner(ownerId: number): Promise<Owner> {
+    return this.ownersService.findOne(ownerId);
   }
 }
